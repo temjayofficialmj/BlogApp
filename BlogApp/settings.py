e65b67a609e31cdffd8f7efd7ed726093ea613d1
temjayofficialmj <temjayofficialmj@gmail.com>
2025-10-01 12:13:48 +0200

    Update settings.py

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -89,13 +89,11 @@ DATABASES = {
     )
 }
 
-# Ensure fallback to SQLite if DATABASE_URL is missing
 if not DATABASES["default"].get("ENGINE"):
     DATABASES["default"] = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
